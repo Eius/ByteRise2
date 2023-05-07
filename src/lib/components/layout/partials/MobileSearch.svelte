7d9bc93994c2a230227715a294d20754535c53db
Eius <34424590+Eius@users.noreply.github.com>
2023-05-07 23:48:05 +0200

    visual bugfix

--- src/lib/components/layout/partials/MobileSearch.svelte
+++ src/lib/components/layout/partials/MobileSearch.svelte
@@ -4,7 +4,7 @@
 	import SearchForm from "./SearchForm.svelte";
 </script>
 
-<div class="block md:hidden bg-primary border-b border-gray-700 {$showSearch ? "h-[50px]" : "h-[0px]"} transition-all duration-300 block lg:hidden">
+<div class="block md:hidden bg-primary border-b md:border-none border-gray-700 -mt-px {$showSearch ? "h-[50px]" : "h-[0px]"} transition-all duration-300 block lg:hidden">
     <div class="container {$showSearch ? "opacity-100" : "opacity-0"} h-full flex gap-2 items-center transition-opacity duration-300">
         <button class="text-accent-1 text-2xl" >
             <Icon icon="material-symbols:search" />
