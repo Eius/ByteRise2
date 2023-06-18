522781fa03d6ac958b06ee37f7ed11d7addc0421
Eius <34424590+Eius@users.noreply.github.com>
2023-06-18 14:13:21 +0200

    Added ADS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,8 @@
 </script>
 
 <svelte:head>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4564448067691193"
+     crossorigin="anonymous"></script>
 	<title>ByteRise | Join the ByteSide</title>
 </svelte:head>
 
