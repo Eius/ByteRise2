2f44f9b846675df65c849bdc24ebadbc24d9130e
Eius <34424590+Eius@users.noreply.github.com>
2023-06-22 22:39:09 +0200

    #37

--- src/routes/article/[slug]/+page.svelte
+++ src/routes/article/[slug]/+page.svelte
@@ -63,10 +63,10 @@
 </div>
 
 <div class="fixed top-1/2 flex items-center rounded z-20 -translate-y-1/2 {openToc ? "right-0" : "-right-[280px]"} transition-all">
-    <button class="bg-neutral rounded-s-3xl -mr-0.5 z-40 border-2 border-r-0 border-base-100" on:click={toggleToc}>
+    <button class="bg-neutral rounded-s-3xl -mr-0.5 z-40 border-2 border-r-0 border-neutral-focus" on:click={toggleToc}>
         <Icon icon="ic:baseline-arrow-left" class="text-5xl text-neutral-content -ml-1 {openToc ? "rotate-180" : ""}" />
     </button>
-    <div class="px-6 py-7 flex flex-col gap-6 bg-neutral h-fit w-[280px] flex-grow rounded-s z-30 border-2 border-base-100">
+    <div class="px-6 py-7 flex flex-col gap-6 bg-neutral h-fit w-[280px] flex-grow rounded-s z-30 border-2 border-r-0 border-neutral-focus">
         <h2 class="text-neutral-content font-bold font-title uppercase">Table of contents</h2>
         <ol class="pr-2 text-neutral-content flex flex-col gap-3 list-decimal list-inside" bind:this={tocDiv}>
             {#each headings as {text, id}}
