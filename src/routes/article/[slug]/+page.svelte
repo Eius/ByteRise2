04582d833263dd28cc741402d48c45e5f52fe026
Eius <34424590+Eius@users.noreply.github.com>
2023-06-22 16:08:58 +0200

    #26

--- src/routes/article/[slug]/+page.svelte
+++ src/routes/article/[slug]/+page.svelte
@@ -66,9 +66,9 @@
     <button class="bg-neutral rounded-s-3xl -mr-2 " on:click={toggleToc}>
         <Icon icon="ic:baseline-arrow-left" class="text-5xl text-neutral-content -ml-1 {openToc ? "rotate-180" : ""}" />
     </button>
-    <div class="px-6 py-7 flex flex-col gap-6 bg-neutral h-fit max-h-[400px] w-[280px] flex-grow rounded-s overflow-y-scroll">
+    <div class="px-6 py-7 flex flex-col gap-6 bg-neutral h-fit w-[280px] flex-grow rounded-s">
         <h2 class="text-neutral-content font-bold font-title uppercase">Table of contents</h2>
-        <ol class="text-neutral-content flex flex-col gap-4 list-decimal list-inside" bind:this={tocDiv}>
+        <ol class="pr-2 text-neutral-content flex flex-col gap-4 list-decimal list-inside" bind:this={tocDiv}>
             {#each headings as {text, id}}
                 <li class="">
                     <a href={`#${id}`}>{text}</a>
